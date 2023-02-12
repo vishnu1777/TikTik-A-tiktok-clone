3286e19c8ced06b7f709347c570316047131e977
Vishnu <vappu5429@gmail.com>
2023-02-12 22:47:24 +0530

    phase-1 commit

---
+++ components/SuggestedAccounts.tsx
@@ -0,0 +1,11 @@
+import type{ NextPage } from "next";
+
+const SuggestedAccounts:NextPage=()=>{
+    return (
+        <div>
+            SuggestedAccounts
+        </div>
+    )
+
+}
+export default SuggestedAccounts;
